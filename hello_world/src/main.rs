131dfeb11dc06166742fce9f832cf6bb1e22190c
PAUL OFFEI <poffei@st.ug.edu.gh>
2022-11-20 16:45:39 +0000

    skimming through basic compound data type

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -33,4 +33,29 @@ fn main() {
         println!("I Love the number {y}");
         y += 1;
     }
+
+    let v = vec![10, 20, 50, 90];
+
+    for item in &v {
+        println!("{item}");
+    }
+
+    println!("{}", v[0]);
+    println!("{}", &v[2]);
+
+    let mut m = 120;
+    let mut n = 170;
+    println!("m is: {m} and n is: {n}");
+    pass_by_reference(&mut m, &mut n);
+    println!("m is: {m} and n is: {n}");
+
+    let bag = (76.7, 88, 'C');
+    let (a, b, c) = bag;
+    println!("my tuples items are: {} {} {}", a, b, c);
+}
+
+fn pass_by_reference(a: &mut i32, b: &mut i32) {
+    let temp: i32 = *a;
+    *a = *b;
+    *b = temp;
 }
